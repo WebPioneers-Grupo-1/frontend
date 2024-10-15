b580a71f801bb3284326d335e9f1ef0d36691f56
DaikiOY <167911449+DaikiOY@users.noreply.github.com>
2024-10-15 17:19:54 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/landingpage/' : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/landingpage/' // Reemplaza 'landingpage' por el nombre de tu repositorio
+    : '/'
 });
